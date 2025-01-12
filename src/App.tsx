989fa3cb159defc43d109a0d9bb968fa2e874067
trbaxter <trb7074@gmail.com>
2025-01-12 15:01:43 -0600

    added drei for orbit controls

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import Earth from './components/Earth';
 import SpaceBackground from "./components/SpaceBackground";
+import { OrbitControls } from "@react-three/drei";
 import './styles/global.css';
 
 const App: React.FC = () => {
@@ -9,10 +10,11 @@ const App: React.FC = () => {
         <div style={{ height: '100vh', width: '100vw', background: 'black', overflow: 'hidden' }}>
             <Canvas>
                 <ambientLight intensity={1} />
-                <pointLight position={[10, 10, 10]} intensity={1}/>
+                <pointLight position={[5, 5, 5]} intensity={1}/>
 
                 <SpaceBackground />
                 <Earth />
+                <OrbitControls />
             </Canvas>
         </div>
     );
