145f11edb7dea4e0a9e1393c69e771b236543a09
trbaxter <trb7074@gmail.com>
2025-01-15 08:28:20 -0600

    Rolling back changes, messes up animation load

--- dart_frontend/src/homepage/components/Earth.tsx
+++ dart_frontend/src/homepage/components/Earth.tsx
@@ -1,29 +1,38 @@
-import { FC, useRef } from 'react';
-import Globe, { GlobeMethods } from 'react-globe.gl';
+import { FC, useEffect, useRef, useState } from 'react';
+import Globe, {GlobeMethods} from 'react-globe.gl';
 import earth_img from '../assets/earth-blue-marble.jpg';
-import earth_topology from '../assets/earth-topology.png';
+import earth_topology from '../assets/earth-topology.png'
 import useCloudLayer from "../hooks/useCloudLayer.ts";
 import useGlobeSetup from "../hooks/useGlobeSetup.ts";
+import useGlobeRadius from "../hooks/useGlobeRadius.ts";
 import useResizeHandler from "../hooks/useResizeHandler.ts";
 
 const Earth: FC = () => {
     const globeElement = useRef<GlobeMethods | undefined>();
-    const CLOUDS_RADIUS = 100;
+    const [globeObj, setGlobeObj] = useState<any>(null);
 
-    useCloudLayer(globeElement.current, CLOUDS_RADIUS);
-    useGlobeSetup(globeElement.current);
-    useResizeHandler(globeElement.current);
+    useEffect(() => {
+        if (globeElement.current) {
+            setGlobeObj(globeElement.current);
+        }
+    }, []);
+
+    const globeRadius = useGlobeRadius(globeObj);
+
+    useCloudLayer(globeObj, globeRadius);
+    useGlobeSetup(globeObj);
+    useResizeHandler(globeObj);
 
     return (
         <Globe
-            ref={globeElement}
-            animateIn={false}
-            globeImageUrl={earth_img}
-            bumpImageUrl={earth_topology}
-            backgroundColor="#000000"
-            showAtmosphere={true}
-            atmosphereAltitude={0.13}
-            waitForGlobeReady={true}
+            ref = { globeElement }
+            animateIn = { false }
+            globeImageUrl = { earth_img }
+            bumpImageUrl = { earth_topology }
+            backgroundColor = { '#000000' }
+            showAtmosphere = { true }
+            atmosphereAltitude= { 0.13 }
+            waitForGlobeReady = { true }
         />
     );
 };
