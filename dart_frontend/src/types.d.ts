175062dc3faa40f11f824b6613c694b94932abea
trbaxter <trb7074@gmail.com>
2025-01-16 12:54:23 -0600

    fixed missing type detection

--- dart_frontend/src/types.d.ts
+++ dart_frontend/src/types.d.ts
@@ -1,6 +1,7 @@
 import { MaterialNode } from '@react-three/fiber';
 import { BrainMaterial } from './aipage/BrainVeins.tsx';
 import { BrainParticleMaterial } from './aipage/BrainParticles.tsx';
+import { ShaderMaterial } from "three";
 
 declare module '@react-three/fiber' {
     interface ThreeElements {
