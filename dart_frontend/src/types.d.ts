48bbb5e781a90791b32755d4cb21a11601cc6462
trbaxter <trb7074@gmail.com>
2025-01-16 12:45:37 -0600

    fixing filepaths

--- dart_frontend/src/types.d.ts
+++ dart_frontend/src/types.d.ts
@@ -1,6 +1,6 @@
 import { MaterialNode } from '@react-three/fiber';
-import { BrainMaterial } from './components/brain-animation/BrainVeins.tsx';
-import { BrainParticleMaterial } from './components/brain-animation/BrainParticles.tsx';
+import { BrainMaterial } from './aipage/BrainVeins.tsx';
+import { BrainParticleMaterial } from './aipage/BrainParticles.tsx';
 
 declare module '@react-three/fiber' {
     interface ThreeElements {
