5ee8ad7a9da273fafc71e7e4512a1543dc779de3
trbaxter <trb7074@gmail.com>
2025-01-16 13:12:35 -0600

    fixed filepaths

--- dart_frontend/src/types.d.ts
+++ dart_frontend/src/types.d.ts
@@ -1,6 +1,6 @@
 import { MaterialNode } from '@react-three/fiber';
-import { BrainMaterial } from './aipage/BrainVeins.tsx';
-import { BrainParticleMaterial } from './aipage/BrainParticles.tsx';
+import { BrainMaterial } from './aipage/components/BrainVeins.tsx';
+import { BrainParticleMaterial } from './aipage/components/BrainParticles.tsx';
 import { ShaderMaterial } from "three";
 
 declare module '@react-three/fiber' {
