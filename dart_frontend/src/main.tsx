e1d20fede132196ba44839730c53db1b9b78657a
trbaxter <trb7074@gmail.com>
2025-01-16 22:10:34 -0600

    checking solution

--- dart_frontend/src/main.tsx
+++ dart_frontend/src/main.tsx
@@ -7,9 +7,11 @@ import App from './App.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter
+        basename="/dart"
         future={{
             v7_startTransition: true,
-            v7_relativeSplatPath: true
+            v7_relativeSplatPath: true,
+
         }}
     >
         <App />
