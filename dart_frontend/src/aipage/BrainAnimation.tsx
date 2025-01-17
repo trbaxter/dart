46dfde2f4c141e2400d96ea2c242da34317bc110
trbaxter <trb7074@gmail.com>
2025-01-17 15:46:01 -0600

    adding <> and removing extensions

--- dart_frontend/src/aipage/BrainAnimation.tsx
+++ dart_frontend/src/aipage/BrainAnimation.tsx
@@ -1,21 +1,23 @@
 import { Canvas } from '@react-three/fiber';
-import { BrainParticles } from './components/BrainParticles.tsx';
-import { Veins } from './components/BrainVeins.tsx';
+import { BrainParticles } from './components/BrainParticles';
+import { Veins } from './components/BrainVeins';
 import { brainCurveVectorArray} from "./utils/brainCurveVectorArray";
-import { RotateGroupFrame } from './components/RotateGroupFrame.tsx';
+import { RotateGroupFrame } from './components/RotateGroupFrame';
 
 export function BrainAnimation() {
     const curves = brainCurveVectorArray();
 
     return (
-        <Canvas camera={{ position: [0, 0, 0.3], near: 0.001, far: 5 }}>
-            <color attach="background" args={['black']} />
-            <ambientLight />
-            <pointLight position={[10, 10, 10]} />
-            <RotateGroupFrame>
-                <Veins curves={curves} />
-                <BrainParticles curves={curves} />
-            </RotateGroupFrame>
-        </Canvas>
+        <>
+            <Canvas camera={{ position: [0, 0, 0.3], near: 0.001, far: 5 }}>
+                <color attach="background" args={['black']} />
+                <ambientLight />
+                <pointLight position={[10, 10, 10]} />
+                <RotateGroupFrame>
+                    <Veins curves={curves} />
+                    <BrainParticles curves={curves} />
+                </RotateGroupFrame>
+            </Canvas>
+        </>
     );
 }
