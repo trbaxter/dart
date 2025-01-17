189947f47a9f310a904fa46b7342ec3be0cba58c
trbaxter <trb7074@gmail.com>
2025-01-17 17:55:21 -0600

    adding text

--- dart_frontend/src/aipage/BrainAnimation.tsx
+++ dart_frontend/src/aipage/BrainAnimation.tsx
@@ -9,15 +9,65 @@ export function BrainAnimation() {
 
     return (
         <>
-            <Canvas camera={{ position: [0, 0, 0.3], near: 0.001, far: 5 }}>
-                <color attach="background" args={['black']} />
+            <Canvas
+                id={'brain'}
+                style={{
+                    position: "relative",
+                    width: '150%',
+                    right: '0'
+                }}
+                camera={{
+                    position: [0, 0, 0.3],
+                    near: 0.001,
+                    far: 5 }}>
+                <color
+                    attach="background"
+                    args={['black']}
+                />
                 <ambientLight />
-                <pointLight position={[10, 10, 10]} />
+                <pointLight
+                    position={[10, 10, 10]}
+                />
                 <RotateGroupFrame>
-                    <Veins curves={curves} />
-                    <BrainParticles curves={curves} />
+                    <Veins
+                        curves={curves}
+                    />
+                    <BrainParticles
+                        curves={curves}
+                    />
                 </RotateGroupFrame>
             </Canvas>
+            <div
+                style = {{
+                    position: 'absolute',
+                    top: '44.5%',
+                    left: '25%',
+                    transform: 'translate(-50%, -50%)',
+                    color: 'white',
+                    textAlign: 'center',
+                    zIndex: 10,
+                }}
+            >
+                <h1 style = {{
+                    fontFamily: 'Playfair Display',
+                    fontSize: '5rem',
+                    fontWeight: 'bold',
+                    userSelect: "none" }}
+                >
+
+                <span
+                    style={{ display: "relative",
+                             marginLeft: '12.5rem' }}>
+                    Harness the brainpower
+                </span>
+                <span
+                    style={{ display: "block",
+                             marginLeft: '12.5rem' }}>
+                    of <span style={{color: '#00E12E'}}>DART AI</span>
+                </span>
+                </h1>
+
+            </div>
         </>
     );
 }
