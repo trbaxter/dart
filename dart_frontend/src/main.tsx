f05cdf440bc3f3956845f48fa96cac90abb47d4e
trbaxter <trb7074@gmail.com>
2025-01-17 10:37:31 -0600

    Removing unnecessary newline

--- dart_frontend/src/main.tsx
+++ dart_frontend/src/main.tsx
@@ -11,7 +11,6 @@ createRoot(document.getElementById('root')!).render(
         future={{
             v7_startTransition: true,
             v7_relativeSplatPath: true,
-
         }}
     >
         <Suspense>
