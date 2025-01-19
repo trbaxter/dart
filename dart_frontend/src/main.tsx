abfc256ec9b54292cb09e441e55cfad8b8c7f51c
trbaxter <trb7074@gmail.com>
2025-01-18 18:26:53 -0600

    removing semicolon

--- dart_frontend/src/main.tsx
+++ dart_frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom"
 import './index.css'
 import App from './App'
 
