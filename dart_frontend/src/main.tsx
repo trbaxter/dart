1b473e96cf4779e4cc2d37b6ffc2a349131ea1e5
trbaxter <trb7074@gmail.com>
2025-01-18 18:09:49 -0600

    removed unneeded comma

--- dart_frontend/src/main.tsx
+++ dart_frontend/src/main.tsx
@@ -9,7 +9,7 @@ createRoot(document.getElementById('root')!).render(
         basename="/dart/"
         future={{
             v7_startTransition: true,
-            v7_relativeSplatPath: true,
+            v7_relativeSplatPath: true
         }}
     >
         <Suspense>
