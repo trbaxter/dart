21a983c124a0935a1b59e298b3909f5f9939e592
trbaxter <trb7074@gmail.com>
2025-01-19 09:19:27 -0600

    cleaning up unnecessary boilerplate

--- dart_frontend/src/homepage/components/Earth/utils/createCloudLayerMesh.ts
+++ dart_frontend/src/homepage/components/Earth/utils/createCloudLayerMesh.ts
@@ -1,35 +1,40 @@
-import { Mesh, MeshPhongMaterial, SphereGeometry, TextureLoader } from 'three';
+import {Mesh, MeshPhongMaterial, SphereGeometry, Texture, TextureLoader} from 'three';
+
+/**
+ * Utility function to load a texture asynchronously.
+ *
+ * @param texturePath - The path to the texture image.
+ * @returns A promise resolving to the loaded texture.
+ */
+const loadTexture = async (texturePath: string): Promise<Texture> => {
+    return new Promise((resolve, reject) => {
+        const textureLoader = new TextureLoader();
+        textureLoader.load(
+            texturePath,
+            (texture) => resolve(texture),
+            undefined,
+            (error) => reject(new Error(`Failed to load texture from ${texturePath}: ${error}`))
+        );
+    });
+};
 
 /**
  * Creates a cloud layer mesh for the globe.
  *
  * @param radius - The radius of the cloud layer.
  * @param texturePath - The path to the cloud texture image.
- * @returns A promise resolving to a `Mesh` representing the cloud layer.
+ * @returns A `Mesh` representing the cloud layer.
  */
-export const createCloudLayerMesh = (
+export const createCloudLayerMesh = async (
     radius: number,
     texturePath: string
 ): Promise<Mesh<SphereGeometry, MeshPhongMaterial>> => {
     const CLOUDS_ALTITUDE = 0.005;
 
-    return new Promise((resolve, reject) => {
-        const textureLoader = new TextureLoader();
+    const cloudsTexture = await loadTexture(texturePath);
 
-        textureLoader.load(
-            texturePath,
-            (cloudsTexture) => {
-                try {
-                    const cloudsMesh = new Mesh(
-                        new SphereGeometry(radius * (1 + CLOUDS_ALTITUDE), 50, 50),
-                        new MeshPhongMaterial({ map: cloudsTexture, transparent: true })
-                    );
-                    resolve(cloudsMesh);
-                } catch (error) {
-                    reject(new Error(`Error creating mesh: ${error}`));
-                }
-            },
-            (error) => reject(new Error(`Failed to load texture from ${texturePath}: ${error}`))
-        );
-    });
+    return new Mesh(
+        new SphereGeometry(radius * (1 + CLOUDS_ALTITUDE), 50, 50),
+        new MeshPhongMaterial({map: cloudsTexture, transparent: true})
+    );
 };
